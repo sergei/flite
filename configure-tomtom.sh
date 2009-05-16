62bc994d9407a79ff13ab5acd5038b2288cdad8b
Sergei <sergei@siriuscybernetics.org>
2009-05-15 18:13:20 -0700

    Added configuration script to build TomTom version

---
+++ configure-tomtom.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+./configure \
+   CC=/usr/local/cross/gcc-3.3.4_glibc-2.3.2/arm-linux/bin/gcc \
+   --prefix=/usr/local/cross/gcc-3.3.4_glibc-2.3.2/arm-linux/ \
+   --build=i386-linux --host=arm-unknown-linux \
+  
+    
